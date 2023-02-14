0aeb9fde5d68e065fb51e23ba2e460242ed0c776
kjw <kjw@youinsystem.com>
2023-02-14 09:12:42 +0900

    update ServletSet

--- TotalOAuthAPI/src/main/java/com/oauth/totaloauthapi/TotalOAuthApiApplication.java
+++ TotalOAuthAPI/src/main/java/com/oauth/totaloauthapi/TotalOAuthApiApplication.java
@@ -2,12 +2,18 @@ package com.oauth.totaloauthapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TotalOAuthApiApplication {
+public class TotalOAuthApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TotalOAuthApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TotalOAuthApiApplication.class, args);
     }
-
 }
